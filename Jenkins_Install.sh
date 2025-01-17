09ef1c4106bdb6bea3d74cef11d3b03b12574027
Manoharpatil-Devops <manohar143patils@gmail.com>
2025-01-17 21:53:37 +0530

    Update Jenkins_Install.sh

--- Jenkins_Install.sh
+++ Jenkins_Install.sh
@@ -1,6 +1,6 @@
-sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-sudo yum upgrade
-sudo yum install jenkins -y
+wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+yum upgrade
+yum install jenkins -y
 systemctl start jenkins
 systemctl status jenkins
